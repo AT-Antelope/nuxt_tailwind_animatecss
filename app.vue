8d2c1e53c53d0eff0a9d22f28a0ca4b6f942c5c4
AT <945322097@qq.com>
2024-12-20 10:11:30 +0800

    style:新增 去除滚动条样式

--- app.vue
+++ app.vue
@@ -25,4 +25,14 @@ body,
 *[class^='animate__'] {
 	animation-duration: 1s;
 }
+
+/*隐藏 Chrome，Safari 和 Opera 的滚动条*/
+.no-scrollbar::-webkit-scrollbar {
+	display: none;
+}
+/*隐藏 IE，Edge 和 Firefox 的滚动条 */
+.no-scrollbar {
+	-ms-overflow-style: none; /* IE 和 Edge */
+	scrollbar-width: none; /* Firefox */
+}
 </style>
